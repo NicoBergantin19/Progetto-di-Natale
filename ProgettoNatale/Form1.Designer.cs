ac6a31399a7fc86971a3f09e0f44740cd97636be
Nicox19 <75247663+Nicox19@users.noreply.github.com>
2021-12-12 10:18:16 +0100

    Inizio progetto

---
+++ ProgettoNatale/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace ProgettoNatale
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProgettoNatale.Properties.Resources.animated_santa_claus;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(524, 168);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(190, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "AZIONI";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(10)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(554, 301);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximumSize = new System.Drawing.Size(570, 340);
+            this.MinimumSize = new System.Drawing.Size(570, 340);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Accesso";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ProgettoNatale/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ProgettoNatale
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connection = new SqlConnection("Data Source=DESKTOP-0JNBS50;Initial Catalog=Natale;Integrated Security=True");
+            try
+            {
+                connection.Open();
+                Nations_Table(connection);
+                Kids_Table(connection);
+                MessageBox.Show("Connessione eseguita correttamente","", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show(error.ToString());
+            }
+        }
+        internal void Nations_Table(SqlConnection connection)
+        {
+            string query = @"SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Nazioni';";
+            SqlCommand controllo = new SqlCommand(query, connection);
+            SqlDataReader reader = controllo.ExecuteReader();
+            if (reader.HasRows == false)
+            {
+                reader.Close();
+                controllo.Cancel();
+                string tab_nazioni = "CREATE TABLE Nazioni(ID int IDENTITY(1,1), Nome varchar(30) NOT NULL, Continente varchar(30), Fuso_Orario datetime, PRIMARY KEY(ID));";
+                SqlCommand cmd = new SqlCommand(tab_nazioni, connection);
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException error)
+                {
+                    MessageBox.Show("Errore nel generare la tabella nazioni: " + error.ToString());
+                }
+            }
+            else
+                reader.Close();                   
+        }
+
+        internal void Kids_Table(SqlConnection connection)
+        {
+            string query = "SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Bambini';";
+            SqlCommand controllo = new SqlCommand(query, connection);
+            SqlDataReader reader = controllo.ExecuteReader();
+            if (reader.HasRows == false)
+            {
+                reader.Close();
+                controllo.Cancel();
+                string tab_nazioni = "CREATE TABLE Bambini(ID int IDENTITY(1,1), Nome varchar(30) NOT NULL,Cognome varchar(30) NOT NULL, Nazione varchar(30), Bonta int PRIMARY KEY(ID));";
+                SqlCommand cmd = new SqlCommand(tab_nazioni, connection);
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException error)
+                {
+                    MessageBox.Show("Errore nel generare la tabella nazioni: " + error.ToString());
+                }
+            }
+            else
+                reader.Close();
+        }
+
+        internal void Insert_Nations(SqlConnection connection)
+        {
+            string insert_nations = "INSERT INTO Nazioni (ID, Nome, Continente, Fuso_Orario) VALUES "
+        }
+    }
+}
